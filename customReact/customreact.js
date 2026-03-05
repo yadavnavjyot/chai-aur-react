962d9f1cfd43e9bad525fae0f2c56f03525703ae
Navjyot Yadav <yadav.navjyot@yahoo.com>
2026-03-05 22:58:41 +0530

    updated code

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,20 +1,21 @@
 function customeRender(reactElement, container) {
-    const domElement = document.createElement(reactElement.type)
-    domElement.innerHTML = reactElement.children
-    for (const prop in reactElement.props) {
-        domElement.setAttribute(prop, reactElement.props[prop])
-    // domElement.setAttribute('href', reactElement.props.href)
-    // domElement.setAttribute('target', reactElement.props.target)
-    container.appendChild(domElement)
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  // domElement.setAttribute('href', reactElement.props.href)
+  // domElement.setAttribute('target', reactElement.props.target)
+  container.appendChild(domElement);
 }
 
 const reactElement = {
-    type: 'a',
-    props: { 
-        href: 'https://google.com',
-        target: '_blank'
-    },
-    children: 'clcik me to visit google'
-}
-const container = document.getElementById('root');
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "clcik me to visit google",
+};
+const container = document.getElementById("root");
 customeRender(reactElement, container);
